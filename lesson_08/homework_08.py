87843da18369c0a0f48f423ef6dea687d83aa876
Руслан <pavlutsky@i.ua>
2025-05-12 19:04:59 +0300

    homework_08.py

---
+++ lesson_08/homework_08.py
@@ -0,0 +1,11 @@
+data = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
+
+
+def summa():
+    for n in data:
+        try:
+            total_sum = sum(int(p) for p in n.split(","))
+            print(total_sum)
+        except ValueError:
+            print("Не можу це зробити")
+summa()
